ac8b61da5e991934183f2e4074474e1fdf49677d
pathompat-m <Pathompat.m@gmail.com>
2023-11-21 10:37:35 +0700

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Pathompat Mekbenchapivat on 21/11/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
